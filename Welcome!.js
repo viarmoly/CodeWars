0e3efe5934b889f092831a307e2b451a8dfd351b
viarmoly <v.iarmolitskyi@gmail.com>
2018-09-22 01:16:40 +0300

    commit #3

---
+++ Welcome!.js
@@ -0,0 +1,26 @@
+function greet(language) {
+    var obj = { // создаем обьект с преветствиями
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'};
+
+    if(obj.hasOwnProperty(language)){ // если искомая пропертя находится в обьекте то с помощью hasOwnPropery мы получаем тру
+        return obj[language];// и получаем значение проперти
+    }else {
+        return obj.english; // в случае отсутвия искомой проперти, мы по умолчанию будем получать значение проперти english
+    }
+}
