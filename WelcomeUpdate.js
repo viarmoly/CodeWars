e50672549e2c53eff0e6d8be3bc6113d1fbcb54c
viarmoly <v.iarmolitskyi@gmail.com>
2018-09-22 01:30:06 +0300

    commit #3.1

---
+++ WelcomeUpdate.js
@@ -0,0 +1,23 @@
+// то самое распространненое решение на CodeWars
+
+function greet(language) {
+    return obj[language] || obj['english']; //  возвращаем значение проперти если такова есть, если нету то по умолчанию Welcome
+}
+
+var obj = {english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'};
