f27e608124709584bc64cb49be811a75c8bec1a9
viarmoly <v.iarmolitskyi@gmail.com>
2018-09-22 01:19:02 +0300

    commit #3

--- Welcome!.js
+++ Welcome!.js
@@ -24,3 +24,7 @@ function greet(language) {
         return obj.english; // в случае отсутвия искомой проперти, мы по умолчанию будем получать значение проперти english
     }
 }
+console.log();
+//Test.assertEquals(greet('english'), 'Welcome', "Your function should have returned 'Welcome'. Try again.");
+//Test.assertEquals(greet('dutch'), 'Welkom', "Your function should have returned 'Welkom'. Try again.");
+//Test.assertEquals(greet('IP_ADDRESS_INVALID'), 'Welcome', "Your function should have returned 'Welcome'. Try again.");
